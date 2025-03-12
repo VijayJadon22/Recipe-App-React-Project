7a30acda08093beb443884adafd00aa58153e4a8
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-12 18:18:42 +0530

    Implemented MobileSidebar component for the responsiveness of the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <>
       <DesktopSidebar />
-      {/* <MobileSidebar /> */}
+      <MobileSidebar />
     </>
   );
 };
@@ -38,6 +38,19 @@ const DesktopSidebar = () => {
           </span>
         </Link>
       </ul>
+    </div>
+  );
+};
+
+const MobileSidebar = () => {
+  return (
+    <div className="flex justify-center gap-10 border-t fixed w-full bottom-0 left-0 bg-white z-10 p-2 sm:hidden">
+      <Link to={"/"}>
+        <Home size={"24"} className="cursor-pointer" />
+      </Link>
+      <Link to={"/favorites"}>
+        <Heart size={"24"} className="cursor-pointer" />
+      </Link>
     </div>
   );
 };
