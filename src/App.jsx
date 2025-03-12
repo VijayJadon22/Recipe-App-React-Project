8f9cebdc254ee7611d57ff7c6a4a715a0e5833db
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-12 18:04:27 +0530

    Added Routes for Homepage and Favoritespage by react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import HomePage from "./pages/HomePage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
   return (
     <div className="flex ">
       <Sidebar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
