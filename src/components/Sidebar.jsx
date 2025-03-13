71f52447d9a5cb8fb531a236a1b5cfb5bbd36cb5
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-13 09:37:52 +0530

    Made some changes in sid ebar to make the content sticky

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const DesktopSidebar = () => {
   return (
     <div className="hidden sm:block p-3 md:p-10 border-r sm:w-24 md:w-64 min-h-screen">
       <div className="flex flex-col gap-20 sticky top-10 left-0">
-        <div className="w-full mb-8">
+        <div className="w-full">
           <img
             src="https://www.pngall.com/wp-content/uploads/8/Cooking-Recipe-PNG-Images.png"
             alt="logo"
@@ -25,19 +25,20 @@ const DesktopSidebar = () => {
           />
           <img src="/mobile-logo.svg" alt="logo" className="block md:hidden" />
         </div>
+
+        <ul className="flex flex-col items-center md:items-start gap-8">
+          <Link to={"/"} className="flex gap-1">
+            <Home size={"24"} />
+            <span className="font-semibold hidden md:block text-md">Home</span>
+          </Link>
+          <Link to={"/favorites"} className="flex gap-1">
+            <Heart size={"24"} />
+            <span className="font-semibold hidden md:block text-md">
+              Favorites
+            </span>
+          </Link>
+        </ul>
       </div>
-      <ul className="flex flex-col items-center md:items-start gap-8">
-        <Link to={"/"} className="flex gap-1">
-          <Home size={"24"} />
-          <span className="font-semibold hidden md:block text-md">Home</span>
-        </Link>
-        <Link to={"/favorites"} className="flex gap-1">
-          <Heart size={"24"} />
-          <span className="font-semibold hidden md:block text-md">
-            Favorites
-          </span>
-        </Link>
-      </ul>
     </div>
   );
 };
