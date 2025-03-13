05470cfef799244a1ae9013a187d5497ddfcd906
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-13 09:37:29 +0530

    Implemented Recipe card to take all the details from the api and display it on the ui

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,40 +1,71 @@
 import { Heart, HeartPulse, Soup } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipe, badge, bg }) => {
+  const [isFavorite, setIsFavorite] = useState(
+    localStorage.getItem("favorites")?.includes(recipe?.idMeal)
+  );
+  const addRecipeToFavorites = () => {
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    const isRecipeAlreadyInFavorites = favorites.some(
+      (fav) => fav.idMeal === recipe.idMeal
+    );
+
+    if (isRecipeAlreadyInFavorites) {
+      favorites = favorites.filter((fav) => fav.idMeal !== recipe.idMeal);
+      setIsFavorite(false);
+    } else {
+      favorites.push(recipe);
+      setIsFavorite(true);
+    }
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
   return (
-    <div className="flex flex-col rounded-md border shadow-md bg-[#ecf7d4] overflow-hidden p-3 relative">
-      <a href="#" className="relative h-32">
+    <div
+      className={`flex flex-col rounded-md border shadow-md ${bg} overflow-hidden p-3 relative`}
+    >
+      <a href={recipe?.strYoutube} target="_blank" className="relative h-32">
         <img
-          src="https://static01.nyt.com/images/2024/08/13/multimedia/Sheet-pan-paprika-lime-salmon-with-corn-salsarex-mpvc/Sheet-pan-paprika-lime-salmon-with-corn-salsarex-mpvc-jumbo-v2.jpg"
+          src={recipe?.strMealThumb}
           alt="recipe image"
           className="rounded-md w-full h-full object-cover cursor-pointer"
         />
         <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-          <Soup size={"16"} /> 4 Servings
+          <Soup size={"16"} /> {recipe?.strMeasure1.split("")[0]} Servings
         </div>
-        <div className="absolute top-1 right-2 bg-white p-1 rounded-full cursor-pointer">
-          <Heart
-            size={"16"}
-            className="hover:fill-red-500 hover:text-red-500"
-          />
+        <div
+          className="absolute top-1 right-2 bg-white p-1 rounded-full cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            addRecipeToFavorites();
+          }}
+        >
+          {!isFavorite && (
+            <Heart
+              size={"16"}
+              className="hover:fill-red-500 hover:text-red-500"
+            />
+          )}
+          {isFavorite && (
+            <Heart size={"16"} className="fill-red-500 text-red-500" />
+          )}
         </div>
       </a>
       <div className="flex mt-1">
-        <p>Roasted Chicken</p>
+        <p>{recipe?.strMeal}</p>
       </div>
-      <p className="my-2">Turkish Kitchen</p>
+      <p className="my-2">{recipe?.strArea}</p>
       <div className="flex gap-2 mt-auto">
-        <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+        <div className={`flex gap-1 ${badge} items-center p-1 rounded-md`}>
           <HeartPulse size={"16"} />
           <span className="text-sm tracking-tighter font-medium">
             Gluten-Free
           </span>
         </div>
-        <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+        <div className={`flex gap-1 ${badge} items-center p-1 rounded-md`}>
           <HeartPulse size={"16"} />
           <span className="text-sm tracking-tighter font-medium">
-            Heart-Healthy
+            {recipe?.strCategory}
           </span>
         </div>
       </div>
